85dc650ec4f56d106da05ec72f847d34de93d2c5
Ihor Diachenko <ih.diachenko@gmail.com>
2021-05-27 02:13:27 +0300

    Handle safari

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 export function checkWebPSupport () {
-    // Check FF, Edge by user agent
-    const match = navigator.userAgent.match(/(Edge|Firefox)\/(\d+)\./)
-    if (match) {
-        return (match[1] === 'Firefox' && +match[2] >= 65)
-            || (match[1] === 'Edge' && +match[2] >= 18)
-    }
-
     // Use canvas hack for webkit-based browsers
     // Kudos to Rui Marques: https://stackoverflow.com/a/27232658/7897049
-    const e = document.createElement('canvas');
-    return e.toDataURL
-        ? e.toDataURL('image/webp').indexOf('data:image/webp') == 0
-        : false
+    const e = document.createElement('canvas')
+    if (e.toDataURL && e.toDataURL('image/webp').indexOf('data:image/webp') == 0) {
+        return true
+    }
+
+    // Check other common browsers by version
+    let m = navigator.userAgent.match(/(Edg|Firefox)\/(\d+)\./)
+    if (m) {
+        return (m[1] === 'Firefox' && m[2] >= 65) || (m[1] === 'Edge' && m[2] >= 18)
+    }
+
+    m = navigator.userAgent.match(/OS X\s?(?<os>\d+)?.+ Version\/(?<v>\d+\.\d+)/)
+    if (m) {
+        return m.groups.v >= 14 && (m.groups.os || 99) >= 11
+    }
+
+    return false
 }
--- supports-webp-app/src/App.vue
+++ supports-webp-app/src/App.vue
@@ -11,9 +11,6 @@ import {checkWebPSupport} from '../..'
 
 const isWebpSupported = checkWebPSupport()
 const userAgent = navigator.userAgent
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
