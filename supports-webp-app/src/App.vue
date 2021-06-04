a43d617e4f9c68dfa3523d1587ff1b08e0795b70
Ihor Diachenko <ih.diachenko@gmail.com>
2021-06-05 02:15:04 +0300

    Beef up the app

--- supports-webp-app/src/App.vue
+++ supports-webp-app/src/App.vue
@@ -1,45 +1,83 @@
 <template>
-  <img class="image" alt="sample webp image" src="./assets/img.webp" />
-  <h1>Does my browser support WebP?</h1>
-  <p class="result" :class="{'success': isWebpSupported, 'failure': !isWebpSupported}">{{ isWebpSupported ? 'YES' : 'NO' }}</p>
-  <p class="footer">{{ userAgent }}</p>
+    <div class="content">
+        <img @load="onLoad(true)" @error="onLoad(false)" class="image" alt="sample webp image" src="./assets/img.webp" />
+        <h1>Does my browser support WebP?</h1>
+        <p class="result" :class="{'success': isWebpSupported, 'failure': !isWebpSupported}">
+        {{ isWebpSupported ? 'YES' : 'NO' }}
+        </p>
+        <h2>
+            <a href="https://www.npmjs.com/package/supports-webp-sync">supports-webp-sync</a> output:
+            <span :class="{'success': isWebpSupportedSync, 'failure': !isWebpSupportedSync}">
+                {{isWebpSupportedSync}}
+            </span>
+        </h2>
+    </div>
+    <div class="footer">
+        {{ userAgent }}
+    </div>
+
 </template>
 
 <script setup>
+import {ref} from 'vue'
 import {checkWebPSupport} from '../..'
 
-
-const isWebpSupported = checkWebPSupport()
+const isWebpSupportedSync = checkWebPSupport()
 const userAgent = navigator.userAgent
+
+const isWebpSupported = ref(null)
+const onLoad = (success) => {
+    isWebpSupported.value =  success
+}
+
 </script>
 
 <style>
+
+html, body {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  display: block;
-  width: 100vw;
-  min-height: 100vh;
+  display: grid;
+  grid-template-areas:
+    "content"
+    "footer";
+  grid-template-rows: 1fr auto;
+  grid-template-columns: 100%;
+  min-height: 100%;
+  max-height: 100%;
+}
+
+.content {
+  grid-area: "content";
+  text-align: center;
 }
 
 .image {
     width: 640px;
     height: auto;
+    margin: 60px;
+}
+
+.result {
+    font-size: 3em;
+    font-weight: bold;
 }
 
 @media (max-width: 600px) {
     .image {
         width: 320px;
     }
-}
 
-.result {
-    font-size: 3em;
-    font-weight: bold;
+    .result {
+        font-size: 2em;
+    }
 }
 
 .success {
@@ -52,7 +90,9 @@ const userAgent = navigator.userAgent
 
 .footer {
     width: 100%;
+    padding: 30px 10px;
     text-align: center;
+    grid-area: "footer";
 }
 
 </style>
