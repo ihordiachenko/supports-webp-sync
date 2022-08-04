1214d451272ea974b88f4dd6af7105fd6b9bfb85
George <zorge.bell@gmail.com>
2022-08-04 21:44:29 +0700

    capture group fix; faster canvas encoding.
    fixed the capturing group for mac os. e.g. it was capturing the '10' instead of '15' in `OS X 10_15_7`.

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ export function  checkWebPSupport(): Boolean  {
     // Use canvas hack for webkit-based browsers
     // Kudos to Rui Marques: https://stackoverflow.com/a/27232658/7897049
     const e = document.createElement('canvas')
+    e.width = 1
+    e.height = 1
     if (e.toDataURL && e.toDataURL('image/webp').indexOf('data:image/webp') == 0) {
         return true
     }
@@ -16,7 +18,7 @@ export function  checkWebPSupport(): Boolean  {
         return (m[1] === 'Firefox' && m[2] >= 65) || (m[1] === 'Edge' && m[2] >= 18)
     }
 
-    m = navigator.userAgent.match(/OS X\s?(?<os>\d+)?.+ Version\/(?<v>\d+\.\d+)/)
+    m = navigator.userAgent.match(/OS X\s?\d*_?(?<os>\d+)?.+ Version\/(?<v>\d+\.\d+)/)
     if (m) {
         return m.groups.v >= 14 && (m.groups.os || 99) >= 11
     }
